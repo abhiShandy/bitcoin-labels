990284a81d7312bc29b0ebd45d6210ba4d26efb5
Abhishek Shandilya <abhishek.sandy@gmail.com>
2022-07-31 10:13:57 -0400

    add few classes to tw safelist

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -7,11 +7,10 @@ export default function Label({
   label: string;
   color: string;
 }) {
-  const borderColor = `border-${color}-500`;
   if (!label) return <></>;
   return (
     <div
-      className={`text-xs px-1.5 py-0.5 mr-1 font-light rounded-xl inline-block text-${color}-500 border ${borderColor}`}
+      className={`text-xs px-1.5 py-0.5 mr-1 font-light rounded-xl inline-block text-${color}-500 border border-${color}-500`}
     >
       {label.toLowerCase()}
     </div>
